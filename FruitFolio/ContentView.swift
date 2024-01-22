c0c592bb7ae77b97114f6ccdc9e5c694c6525f68
Abhyuday Reddy Nandikonda <abhyudayreddy1534@gmail.com>
2024-01-22 14:12:14 -0600

    Initial Commit

---
+++ FruitFolio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FruitFolio
+//
+//  Created by Sravanthi Chinthireddy on 22/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FruitFolio/FruitFolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitFolioApp.swift
+//  FruitFolio
+//
+//  Created by Sravanthi Chinthireddy on 22/01/24.
+//
+
+import SwiftUI
+
+@main
+struct FruitFolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
